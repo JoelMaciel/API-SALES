6f846e3b63702e998db9da13dafb0407f5abd49d
Joel Maciel <soldado25461@gmail.com>
2023-02-08 15:57:25 -0300

    feat: creating service de customer, update, list, delete, byId, create

---
+++ src/modules/customers/services/ShowCustomerService.ts
@@ -0,0 +1,22 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
+import CustomersRepository from '../typeorm/repositories/CustomersRepository';
+
+interface IResquest {
+  id: string;
+}
+
+class ShowCustomerService {
+  public async execute({ id }: IResquest): Promise<Customer> {
+    const customerRepository = getCustomRepository(CustomersRepository);
+    const customer = await customerRepository.findById(id);
+
+    if (!customer) {
+      throw new AppError('Customer not found.');
+    }
+    return customer;
+  }
+}
+
+export default ShowCustomerService;
