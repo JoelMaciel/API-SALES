1dd733c5c7d07d1257bfa86dd9ea9787959f0145
Joel Maciel <soldado25461@gmail.com>
2023-02-15 13:40:10 -0300

    feat: S3 Storage provider

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,6 +1,20 @@
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import path from 'path';
 import crypto from 'crypto';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tempFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
 const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
@@ -8,14 +22,22 @@ const tempFolder = path.resolve(__dirname, '..', '..', 'temp');
 export default {
   directory: uploadFolder,
   tempFolder,
-  storage: multer.diskStorage({
-    destination: tempFolder,
-    filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('hex');
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
 
-      const filename = `${fileHash}-${file.originalname}`;
+        const filename = `${fileHash}-${file.originalname}`;
 
-      callback(null, filename);
+        callback(null, filename);
+      },
+    }),
+  },
+
+  config: {
+    aws: {
+      bucket: 'api-sales01',
     },
-  }),
-};
+  },
+} as IUploadConfig;
